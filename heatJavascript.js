62091cee9bedc46a63821b108953d9fb2b586d38
Katy Nguyen <katymnletters@gmail.com>
2024-08-07 14:03:55 -0500

    Added basic css to title page and reconfigured html

---
+++ heatJavascript.js
@@ -0,0 +1,23 @@
+const diseases = [
+    {
+        name:"Heat ",
+        symptoms: ["","",""],
+        description: "",
+        care: "",
+        image:""
+    },
+    {
+        name:"Heat ",
+        symptoms: ["","",""],
+        description: "",
+        care: "",
+        image:""
+    },
+    {
+        name:"Heat ",
+        symptoms: ["","",""],
+        description: "",
+        care: "",
+        image:""
+    },
+]
