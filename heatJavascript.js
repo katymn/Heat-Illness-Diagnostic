3b85b59d677fd3114f70059b4eb90d722ad88652
Katy Nguyen <katymnletters@gmail.com>
2024-08-07 19:44:44 -0500

    made website responsive

--- heatJavascript.js
+++ heatJavascript.js
@@ -1,13 +1,13 @@
 const diseases = [
     {
-        name:"Heat ",
-        symptoms: ["","",""],
-        description: "",
-        care: "",
+        name:"Heat Stroke",
+        symptoms: ["High body temperature (above 104°F or 40°C)","",""],
+        description: "Heat stroke is a severe condition characterized by a body temperature above 104°F (40°C), altered mental state or behavior, and potential damage to internal organs. It is the most serious form of heat injury and can be life-threatening without prompt treatment.",
+        care: "Immediate cooling is critical; move the patient to a cooler environment, remove excess clothing, and use ice packs or cool water to lower body temperature. Seek emergency medical help as soon as possible, and monitor the patient's breathing and heart rate until help arrives.",
         image:""
     },
     {
-        name:"Heat ",
+        name:"Heat Exhaustion",
         symptoms: ["","",""],
         description: "",
         care: "",
