15f3ca4044753a1ca4c64ba0d9bc8ec26e8ec712
Katy Nguyen <katymnletters@gmail.com>
2024-08-10 18:14:50 -0500

    fixed responsiveness and added button display function

--- heatJavascript.js
+++ heatJavascript.js
@@ -16,19 +16,19 @@ const diseases = [
         name:"Heat Stroke",
         symptoms: ["high body temperature (above 104°F or 40°C)","altered mental state or confusion","hot, dry skin or profuse sweating"],
         care: "Immediate cooling is critical; move the patient to a cooler environment, remove excess clothing, and use ice packs or cool water to lower body temperature. Seek emergency medical help as soon as possible, and monitor the patient's breathing and heart rate until help arrives.",
-        video: `<iframe width="100%" height="315" src="https://www.youtube.com/embed/jvGC_dQJUtE?si=Tf3vkM88thmnk_yJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+        video: `<iframe width="100%" height="200" src="https://www.youtube.com/embed/jvGC_dQJUtE?si=Tf3vkM88thmnk_yJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
     },
     {
         name:"Heat Exhaustion",
         symptoms: ["heavy sweating","weakness or fatigue","nausea or vomiting"],
         care: "Move the patient to a cool, shaded area and have them lie down with their legs elevated. Provide cool water or an electrolyte-rich drink and encourage rest until symptoms subside; seek medical attention if symptoms worsen or persist.",
-        video: `<iframe width="100%" height="315" src="https://www.youtube.com/embed/R6VdoV8dZRc?si=coULmOoilhQ8rQaj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
+        video: `<iframe width="100%" height="200" src="https://www.youtube.com/embed/R6VdoV8dZRc?si=coULmOoilhQ8rQaj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
     },
     {
         name:"Heat Cramps",
         symptoms: ["painful muscle spasms, usually in the legs, arms, or abdomen","heavy sweating during intense exercise","muscle pain or tightness"],
         care: "Stop all physical activity and move to a cooler environment. Drink water or a sports drink with electrolytes, and gently stretch and massage the affected muscles until the cramps subside.",
-        video: `<iframe width="100%" height="315" src="https://www.youtube.com/embed/sO-V5Uj-dYg?si=b3BViBlvdbOcfVs5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+        video: `<iframe width="100%" height="200" src="https://www.youtube.com/embed/sO-V5Uj-dYg?si=b3BViBlvdbOcfVs5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
     },
     {
         name:"Heat Tetany",
@@ -46,7 +46,7 @@ const diseases = [
         name:"Heat Rash",
         symptoms: ["red clusters of small blisters","itching or prickling sensation","rash in areas where sweat is trapped (e.g., underarms, neck, chest, groin)"],
         care: "Keep the affected area cool, dry, and exposed to air. Apply calamine lotion or hydrocortisone cream to relieve itching, and avoid further sweating by staying in a cool environment and wearing loose, breathable clothing.",
-        video: `<iframe width="100%" height="315" src="https://www.youtube.com/embed/FReMtVzk55M?si=V8S6VPkQka4xPOo6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
+        video: `<iframe width="100%" height="200" src="https://www.youtube.com/embed/FReMtVzk55M?si=V8S6VPkQka4xPOo6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
     }
 ]
 
@@ -125,6 +125,7 @@ function diagnosis() {
     h1.innerHTML = `${diseases[pageNumber].name}`;
     desc1.innerHTML = `Your patient may be experiencing <strong>${diseases[pageNumber].name}</strong>. <br><br>${diseases[pageNumber].name} is characterized by ${diseases[pageNumber].symptoms.join(", ")}`
     desc2.innerHTML = `${diseases[pageNumber].care}`;
+    
     displayRestart();
 
     if (pageNumber == 3 || pageNumber == 4) {
@@ -144,15 +145,17 @@ function restart() {
     img.src = "https://thumb.ac-illust.com/78/78c0d398f063e281134cdb6383797875_t.jpeg";
     img.alt = "Male worker sweating in the sun while holding a water bottle and a handkerchief";
     img.style.display = "inline-flex"
-    yesButton.style.display = "none";
-    noButton.style.display = "none";
+    buttonDisplay(beginButton, yesButton, noButton);
     restartButton.style.display = "none";
-    beginButton.style.display = "inline-flex";
 }
 
 function displayRestart() {
     img.style.display = "none";
-    yesButton.style.display = "none";
-    noButton.style.display = "none";
-    restartButton.style.display = "inline-flex";
+    buttonDisplay(restartButton, yesButton, noButton);
+}
+
+function buttonDisplay(buttonDisplay,b1,b2) {
+    buttonDisplay.style.display = "inline-flex";
+    b1.style.display = "none";
+    b2.style.display = "none";
 }
