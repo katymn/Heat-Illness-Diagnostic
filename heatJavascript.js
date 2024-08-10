6c0ac6d0245581848684d213fa610d74f1db5b55
Katy Nguyen <katymnletters@gmail.com>
2024-08-10 17:32:01 -0500

    add videos

--- heatJavascript.js
+++ heatJavascript.js
@@ -15,43 +15,37 @@ const diseases = [
         name:"Heat Stroke",
         symptoms: ["high body temperature (above 104°F or 40°C)","altered mental state or confusion","hot, dry skin or profuse sweating"],
         care: "Immediate cooling is critical; move the patient to a cooler environment, remove excess clothing, and use ice packs or cool water to lower body temperature. Seek emergency medical help as soon as possible, and monitor the patient's breathing and heart rate until help arrives.",
-        image:"",
-        alt: ""
+        video: `<iframe width="100%" height="340px" src="https://www.youtube.com/embed/jvGC_dQJUtE?si=Tf3vkM88thmnk_yJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
     },
     {
         name:"Heat Exhaustion",
         symptoms: ["heavy sweating","weakness or fatigue","nausea or vomiting"],
         care: "Move the patient to a cool, shaded area and have them lie down with their legs elevated. Provide cool water or an electrolyte-rich drink and encourage rest until symptoms subside; seek medical attention if symptoms worsen or persist.",
-        image:"",
-        alt: ""
+        video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/R6VdoV8dZRc?si=coULmOoilhQ8rQaj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
     },
     {
         name:"Heat Cramps",
         symptoms: ["painful muscle spasms, usually in the legs, arms, or abdomen","heavy sweating during intense exercise","muscle pain or tightness"],
         care: "Stop all physical activity and move to a cooler environment. Drink water or a sports drink with electrolytes, and gently stretch and massage the affected muscles until the cramps subside.",
-        image:"",
-        alt: ""
+        video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/sO-V5Uj-dYg?si=b3BViBlvdbOcfVs5" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
     },
     {
         name:"Heat Tetany",
         symptoms: ["tingling or numbness in the extremities","muscle spasms or cramps","hyperventilation"],
         care: "Move the patient to a cooler area and encourage slow, controlled breathing to alleviate hyperventilation. Hydrate with cool fluids, and if symptoms persist, seek medical evaluation for further assessment and treatment.",
-        image:"",
-        alt: ""
+        link: "https://www.icliniq.com/articles/diseases-and-disorders-common-medical-conditions/heat-tetany"
     },
     {
         name:"Heat Edema",
         symptoms: ["swelling in hands, ankles, or feet","mild discomfort","increased swelling with prolonged standing"],
         care: "Elevate the affected limbs to reduce swelling and avoid standing for long periods in the heat. Ensure adequate hydration and consider using compression stockings if the swelling is significant or persistent.",
-        image:"",
-        alt: ""
+        link: "https://thekimmelinstitute.com/could-your-swelling-be-heat-edema/"
     },
     {
         name:"Heat Rash",
         symptoms: ["red clusters of small blisters","itching or prickling sensation","rash in areas where sweat is trapped (e.g., underarms, neck, chest, groin)"],
         care: "Keep the affected area cool, dry, and exposed to air. Apply calamine lotion or hydrocortisone cream to relieve itching, and avoid further sweating by staying in a cool environment and wearing loose, breathable clothing.",
-        image:"",
-        alt: ""
+        video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/FReMtVzk55M?si=V8S6VPkQka4xPOo6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`,
     }
 ]
 
@@ -132,9 +126,12 @@ function diagnosis() {
     desc2.innerHTML = `${diseases[pageNumber].care}`;
     img.src = "";
     displayRestart();
-    const video = `
-    <iframe id="videoPlayer" width="560" height="315" src="https://www.youtube.com/watch?v=Qog3GveRj40" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
-    moreContent.innerHTML += video;
+    if (3 <= pageNumber <= 4) {
+        const link = <a ></a>
+    }
+    else {
+        const video = `${diseases[pageNumber].video}`;
+        moreContent.innerHTML += video;}
 }
 
 function restart() {
@@ -143,6 +140,7 @@ function restart() {
     desc2.innerHTML = `This tool is for educational purposes only, not for diagnosis.<br><br>Press begin to identify symptoms and their corresponding heat illness.`
     img.style.display = "inline-flex"
     img.src = "https://thumb.ac-illust.com/78/78c0d398f063e281134cdb6383797875_t.jpeg";
+    img.alt = "Male worker sweating in the sun while holding a water bottle and a handkerchief";
     yesButton.style.display = "none";
     noButton.style.display = "none";
     restartButton.style.display = "none";
